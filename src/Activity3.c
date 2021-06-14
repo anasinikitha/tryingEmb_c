c4163f43f44d35fe921824fec86597946d363707
anasinikitha <67320294+anasinikitha@users.noreply.github.com>
2021-06-14 22:02:53 +0530

    Update Activity3.c

--- src/Activity3.c
+++ src/Activity3.c
@@ -1,9 +1,16 @@
-#include"Activity3.h"
- 
- void InitPwm()
- {
-      TCCR0A|=(1<<COM0A1)|(1<<WGM00)|(1<<WGM01);
-      TCCR0B|=(1<<CS00)|(1<<CS01);
-      DDRD|=(1<<PD6);
-      OCR0A=255;
- }
+#ifndef __AVR_ATmega328__
+#define __AVR_ATmega328__
+#endif
+
+#include "activity1.h"
+#include "project_config.h"
+#include "activity2.h"
+#include "activity3.h"
+
+void InitPWM()
+{
+    TIMER_CONTROL_REGISTER_A|=(1<<NON_INVERTING_COMPAIR_MODE)|(1<<WAVE_GENERATION_MODE_0)|(1<<WAVE_GENERATION_MODE_1);// nON INVERTING PWM & FAST  PWM MODE
+    TIMER_CONTROL_REGISTER_B|=(1<<PRESCALAR_BIT_0)|(1<<PRESCALAR_BIT_1);//PRESCALING OF /64
+    DDRD|=(1<<CRO_DISPLAY_PIN);
+    OUTPUT_COMPAIR_REGISTER_0A=255;
+}
