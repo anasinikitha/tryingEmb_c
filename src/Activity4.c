d02d79b91988dd3a76639ad2b10f28e31dfc319f
anasinikitha <67320294+anasinikitha@users.noreply.github.com>
2021-06-14 22:03:43 +0530

    Update Activity4.c

--- src/Activity4.c
+++ src/Activity4.c
@@ -1,22 +1,40 @@
-#include"Activity4.h"
+#ifndef __AVR_ATmega328__
+#define __AVR_ATmega328__
+#endif
+
+#include "activity1.h"
+#include "project_config.h"
+#include "activity2.h"
+#include "activity3.h"
+#include "activity4.h"
 
 void USARTWriteData(uint16_t data)
 {
- //wait until transmitter is ready
-while(!(UCSR0A & (1<<UDRE0)))
- {
-   //DO NOTHING
-  }
-  // NOW WRITE DATA TO USART BUFFER
-  UDR0=data;
+    //wait until transmitter is ready
+    while(!(USART_CONTROL_STATUS_REG_0A & (1<<USART_DATA_REG_EMPTY)))
+    {
+        //do nothing
+    }
+    //write data to USART buffer
+    USART_DATA_REG0=data;
 }
+
 void USARTInit(uint16_t ubrr_value)
 {
- //set Baud rate
- UBRR0L= ubrr_value;
- UBRR0H=(ubrr_value>>8)&(0x00ff);
-UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
+    //set Baud rate
+    USART_BAUDRATE_REG_L=ubrr_value;
+    USART_BAUDRATE_REG_H=(ubrr_value>>8)&(0x00ff);
+    USART_CONTROL_STATUS_REG_0B=(1<<RECEIVER_COMPLETE_INTERUPT_ENABLE)|(1<<TRANSMITTER_COMPLETE_INTERUPT_ENABLE)|(1<<TRANSMITTER_ENABLE)|(1<<RECEIVER_ENABLE);
+    USART_CONTROL_STATUS_REG_0C=(1<<USART_MODE_SELECT)|(1<<USART_DATA_CHAR_SIZE_BIT1)|(1<<USART_DATA_CHAR_SIZE_BIT0);
 
-//ENABLE RECEIVER AND TRANSMITTER
- UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 }
+
+// uint16_t USARTReadData()
+// {
+//     //WAIT UNTIL DATA IS AVAILABLE
+//     while(!(USART_CONTROL_STATUS_REG_0A & (1<<USART_RECEIVE_COMPLETE)))
+//     {
+//         //do nothing
+//     }
+//     return USART_DATA_REG0;
+// }
