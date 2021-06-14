7270428cea09f8c55ddbdb0607cc5ff3f9621efd
anasinikitha <67320294+anasinikitha@users.noreply.github.com>
2021-06-14 22:00:57 +0530

    Update Activity1.c

--- src/Activity1.c
+++ src/Activity1.c
@@ -1,11 +1,29 @@
-#include "Activity1.h"
+#ifndef __AVR_ATmega328__
+#define __AVR_ATmega328__
+#endif
+
+#include "activity1.h"
+#include "project_config.h"
+#include "activity2.h"
+#include "activity3.h"
+#include "activity4.h"
 
 
-void delay_ms(uint32_t delay_time)
+void peripheral_init(void)
 {
-	uint32_t units = 0;
-	for (units = 0; units <= delay_time; units++)
-	{
-		_delay_ms(1);
-	}
+	DDRB|=(1<<LED_PIN);//SET PB0 AS OUTPUT PIN FOR LED
+    DDRD&=~(1<<HEARTER_PIN);//PUSH BUTTON FOR PASENGER CHECK
+    DDRD&=~(1<<OCCUPANCY_PIN);//PUSH BUTTON FOR HEATER CHECK
+
+    HEATER_OCCUPANCY_PORT|=(1<<HEARTER_PIN);//SET 5 VOLT WHEN SWITCH IS OFF
+    HEATER_OCCUPANCY_PORT|=(1<<OCCUPANCY_PIN);//SET 5 VOLT WHEN SWITCH IS OFF
+
+    InitADC();//INITALIZE ADC
+    InitPWM(); //INITIALIZATION FOR PWM OUTPUT
+    USARTInit(103); //USART INITIATE
+}
+
+void change_led_state(uint8_t state)
+{
+	LED_PORT = (state << LED_PIN);
 }
